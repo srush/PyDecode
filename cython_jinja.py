6b5513aa571f077f5face86680874142ce4b41b8
Sasha Rush <sasha.rush@gmail.com>
2014-04-14 11:47:00 -0400

    Move to template

--- cython_jinja.py
+++ cython_jinja.py
@@ -11,11 +11,11 @@ for var in vars["semirings"]:
 
 template = env.get_template('potentials.pyx.tpl')
 out = open("python/pydecode/potentials.pyx", "w")
-print >>out, open("python/pydecode/libhypergraph.pyx").read()
+print >>out, open("python/pydecode/templates/libhypergraph.pyx").read()
 print >>out, template.render(vars)
 
 
 template = env.get_template('potentials.pxd.tpl')
 out = open("python/pydecode/potentials.pxd", "w")
-print >>out, open("python/pydecode/libhypergraph.pxd").read()
+print >>out, open("python/pydecode/templates/libhypergraph.pxd").read()
 print >>out, template.render(vars)
