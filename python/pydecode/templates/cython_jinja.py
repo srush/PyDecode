f00668e90224cf78375f65e8be27b2a9d968946b
Sasha Rush <sasha.rush@gmail.com>
2014-04-30 10:25:05 -0400

    Makefile

--- python/pydecode/templates/cython_jinja.py
+++ python/pydecode/templates/cython_jinja.py
@@ -12,10 +12,12 @@ for var in vars["semirings"]:
 template = env.get_template('potentials.pyx.tpl')
 out = open("python/pydecode/potentials.pyx", "w")
 print >>out, open("python/pydecode/templates/libhypergraph.pyx").read()
+print >>out, open("python/pydecode/templates/beam.pyx").read()
 print >>out, template.render(vars)
 
 
 template = env.get_template('potentials.pxd.tpl')
 out = open("python/pydecode/potentials.pxd", "w")
 print >>out, open("python/pydecode/templates/libhypergraph.pxd").read()
+print >>out, open("python/pydecode/templates/beam.pxd").read()
 print >>out, template.render(vars)
